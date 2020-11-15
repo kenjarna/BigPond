8ac30a143236334c39f628652bb0025cae4108d9
Kenny Jarnagin <jarnagink19@hanover.edu>
2020-11-15 00:27:50 -0500

    Create initial logic for fetching twitch channel details

--- bigpond/src/LogicComponents/getStreamerDetails.js
+++ bigpond/src/LogicComponents/getStreamerDetails.js
@@ -1,25 +1,83 @@
 import axios from 'axios';
-import {getStreamerBase} from '../config.js';
+
+const twitchAuthAPI = 'https://id.twitch.tv/oauth2/token';
+const streamerDetailsAPI = 'https://api.twitch.tv/helix/search/channels';
+const streamsAPI = 'https://api.twitch.tv/helix/streams';
+
+const client_id = process.env.REACT_APP_CLIENT_ID;
+const client_secret = process.env.REACT_APP_CLIENT_SECRET;
+const grant_type = process.env.REACT_APP_GRANT_TYPE;
 
 export default class StreamerDetails {
     constructor(first=20){
+        
         this.first = first;
+        this.authToken = undefined;
+        this.streamerData = undefined;
     }
 
-    buildLink(streamerName){
-        return getStreamerBase + "?query=" + streamerName;
+    async getStreamerDetails(streamerName=""){
+        await this.getAuthorizationToken();
+        if(streamerName.match("$^") || streamerName === undefined){
+            await this.getRandomStreamDetails(this.getAuthToken());
+        } else{
+            await this.getSpecificStream(streamerName,this.getAuthToken());
+        }
+    }
+
+    async getRandomStreamDetails(authToken) {
+        const Authorization = "Bearer " + authToken;
+        await axios.get(streamsAPI, { 
+            params:{
+                'first': 100
+            },
+            headers:{
+                'client-id':client_id,
+                Authorization
+            }
+        })
+        .then(response => this.setStreamerData(response.data));
+    }
+
+    async getSpecificStream(streamerName,authToken){
+        this.getAuthorizationToken();
+        const Authorization = "Bearer " + authToken;
+
+        await axios.get(streamerDetailsAPI+"?query="+streamerName, { 
+
+            headers: {
+                'client-id': client_id,
+                Authorization
+            }})
+        .then(response => this.setStreamerData(response.data));
+    }
+
+    async getAuthorizationToken() {
+        
+        await axios.post(twitchAuthAPI ,null, { params: {
+            client_id,
+            client_secret,
+            grant_type
+        }})
+        .then(response => this.setAuthToken(response.data.access_token));
+    }
+
+    setAuthToken(token){
+        this.authToken = token;
     }
 
     getAuthToken() {
-
+        return this.authToken;
     }
 
-
-    async fetchStreamerInfo(){
-        const link = this.buildLink();
-        const authToken = this.getAuthToken();
-
+    setStreamerData(data){
+        this.streamerData = data;
+    
     }
 
+    getStreamerData(){
+
+        return this.streamerData;
+    }
 
 }
